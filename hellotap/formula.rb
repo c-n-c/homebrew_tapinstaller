62fc76c104be3e66f5d66d1c3e294e8d0c80d39b
gocd user <gocd_user@someorg.com>
2020-08-06 02:10:29 +0530

    hellotap demo installation repository for homebrew with makefile and gocd template

---
+++ hellotap/formula.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+
+class Hellotap < Formula
+  desc "Installation of demo hellotap"
+  homepage "https://abcdbank.com/"
+  url "URI"
+  version "1.0"
+  sha256 "SHASUM"
+
+  bottle :unneeded
+
+  # depends_on "cmake" => :build
+
+  def install
+    prefix.install "testfile.txt"
+    system "bash", "install.sh"
+    system "make", "install"
+  end
+
+  # test do
+  #   # `test do` will create, run in and delete a temporary directory.
+  #   #
+  #   # This test will fail and we won't accept that! For Homebrew/homebrew-core
+  #   # this will need to be a test that verifies the functionality of the
+  #   # software. Run the test with `brew test certs`. Options passed
+  #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+  #   #
+  #   # The installed folder is not in the path, so use the entire path to any
+  #   # executables being tested: `system "#{bin}/program", "do", "something"`.
+  #   system "false"
+  # end
+end
--- hellotap/install.sh
+++ hellotap/install.sh
@@ -4,9 +4,3 @@ echo "#                                           #"
 echo "#            hello world !                  #"
 echo "#                                           #"
 echo "#############################################"
-
-echo "hello world" > /tmp/hello_world
-cat /tmp/hello_world
-if [ -f /usr/bin/say and -f /tmp/hello_world ];then
-    say "hello world ! welcome to the brew tap installer example by Dev Ops"
-fi
