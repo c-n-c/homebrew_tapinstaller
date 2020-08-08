399fb6edfea89783fa91ebfc1bf9d5ef883da6cb
gocd user <gocd_user@someorg.com>
2020-08-08 17:28:09 +0530

    write test for the hellotap formula

--- hellotap/formula.rb
+++ hellotap/formula.rb
@@ -18,16 +18,7 @@ class Hellotap < Formula
     system "make", "install"
   end
 
-  # test do
-  #   # `test do` will create, run in and delete a temporary directory.
-  #   #
-  #   # This test will fail and we won't accept that! For Homebrew/homebrew-core
-  #   # this will need to be a test that verifies the functionality of the
-  #   # software. Run the test with `brew test certs`. Options passed
-  #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-  #   #
-  #   # The installed folder is not in the path, so use the entire path to any
-  #   # executables being tested: `system "#{bin}/program", "do", "something"`.
-  #   system "false"
-  # end
+  test do
+    assert_match revision.to_s, shell_output("echo #{prefix}/textfile.txt")
+  end
 end
